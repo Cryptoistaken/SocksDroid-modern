4f30cab82394aa7ed5f9d8c67eda9fd2324ba54f
Cryptoistaken <traderspopy@gmail.com>
2026-07-15 18:01:41 +0600

    Fix: Add missing luminance() import in StatusScreen

--- app/src/main/java/net/typeblog/socks/ui/screens/StatusScreen.kt
+++ app/src/main/java/net/typeblog/socks/ui/screens/StatusScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
